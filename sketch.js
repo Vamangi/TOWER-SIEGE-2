ad770b4055b730851c6bd2abf3761fe6736b7828
Vamangi <75316070+Vamangi@users.noreply.github.com>
2021-02-06 20:55:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,186 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+
+var engine, world;
+var ball
+var gronud,stand1
+var polygon_img
+
+function setup() {
+  createCanvas(1350,645);
+  engine = Engine.create();
+  world = engine.world;
+
+
+  stroke(255)
+
+
+  
+
+  ball = Bodies.circle(50,200,20);
+  World.add(world,ball);
+
+  ground=new Ground(1,635,3000,4)
+  stand1=new Ground(590,500,250,10)
+  stand2=new Ground(1000,200,200,10)
+   //level one
+   block1 = new Block(500,475,30,40);
+  
+   block2 = new Block(530,475,30,40);
+   block3 = new Block(560,475,30,40);
+   block4 = new Block(590,475,30,40);
+   block5 = new Block(620,475,30,40);
+   block6 = new Block(650,475,30,40);
+   block7 = new Block(680,475,30,40);
+   //level two
+   block8 = new Block(530,435,30,40);
+   block9 = new Block(560,435,30,40);
+   block10 = new Block(590,435,30,40);
+   block11 = new Block(620,435,30,40);
+   block12 = new Block(650,435,30,40);
+   //level three
+   block13 = new Block(560,395,30,40);
+   block14 = new Block(590,395,30,40);
+   block15 = new Block(620,395,30,40);
+   //top
+   block16 = new Block(590,355,30,40);
+ 
+   //set 2 for second stand
+   //level one
+   blocks1 = new Block(940,175,30,40);
+   blocks2 = new Block(970,175,30,40);
+   blocks3 = new Block(1000,175,30,40);
+   blocks4 = new Block(1030,175,30,40);
+   blocks5 = new Block(1060,175,30,40);
+   //level two
+   blocks6 = new Block(970,135,30,40);
+   blocks7 = new Block(1000,135,30,40);
+   blocks8 = new Block(1030,135,30,40);
+   //top
+   blocks9 = new Block(1000,95,30,40);
+ 
+
+
+
+ 
+  sling=new Slingshot(this.ball,{x:180,y:400})
+
+
+}
+
+function preload(){
+  polygon_img=loadImage("polygon.png");
+}
+
+function draw() {
+  //camera.zoom=camera.zoom+1
+  background(0);  
+  Engine.update(engine);
+  strokeWeight(4);
+  stroke(15);
+  textSize(35);
+  fill("white");
+  text("Drag the Hexagonal Stone and Release it, to launch it towards the blocks",100,30);
+
+  fill("white");
+  text("Press 'SPACE KEY' to get a second chance,",100,630);
+
+
+ground.display()
+stand1.display()
+stand2.display()
+strokeWeight(4);
+  stroke(15);
+  fill("blue");
+  block1.display();
+  fill("pink");
+
+  block2.display();
+  block3.display();
+
+  fill("red");
+  block4.display();
+
+  fill("yellow");
+
+  block5.display();
+  fill("green");
+
+  block6.display();
+
+  block7.display();
+  fill("puple");
+  block8.display();
+  fill("brown");
+
+  block9.display();
+  fill("orange");
+
+  block10.display();
+  fill("pink");
+
+  block11.display();
+  fill("blue");
+
+  block12.display();
+  fill("green");
+  block13.display();
+  fill("yellow");
+
+  block14.display();
+  fill("blue");
+
+  block15.display();
+  fill("orange");
+  block16.display();
+  fill("skyblue");
+  blocks1.display();
+  fill("red");
+
+  blocks2.display();
+  fill("red");
+
+  blocks3.display();  
+  fill("grey");
+
+  blocks4.display();
+  fill("turquoise");
+
+  blocks5.display(); 
+   fill("purple");
+
+  blocks6.display();
+  fill("yellow");
+
+  blocks7.display();
+  blocks8.display();
+  fill("pink")
+  blocks9.display();
+
+  sling.display()
+
+imageMode(CENTER)
+  image(polygon_img ,ball.position.x,ball.position.y,40,40);
+ drawSprites();
+}
+
+
+
+
+
+function mouseDragged(){
+  Matter.Body.setPosition(this.ball,{x:mouseX,y:mouseY});
+}
+function mouseReleased(){
+  sling.fly();
+}
+
+
+function keyPressed(){
+  if(keyCode===32){
+      sling.attach(this.ball)
+  }
+}
